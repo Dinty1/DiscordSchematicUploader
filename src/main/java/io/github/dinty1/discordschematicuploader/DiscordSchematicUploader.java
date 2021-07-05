6f7ac6aaacc4ee5ea0c85b9a3260b1bec60e754b
Dinty1 <dintybmrt@gmail.com>
2021-07-05 10:47:49 +0100

    Add metrics

--- src/main/java/io/github/dinty1/discordschematicuploader/DiscordSchematicUploader.java
+++ src/main/java/io/github/dinty1/discordschematicuploader/DiscordSchematicUploader.java
@@ -2,11 +2,16 @@ package io.github.dinty1.discordschematicuploader;
 
 import github.scarsz.discordsrv.DiscordSRV;
 import io.github.dinty1.discordschematicuploader.listener.DiscordGuildMessagePreProcessListener;
+import io.github.dinty1.discordschematicuploader.metrics.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DiscordSchematicUploader extends JavaPlugin {
+
+    private final int METRICS_PLUGIN_ID = 11934;
+
     @Override
     public void onEnable() {
+        Metrics metrics = new Metrics(this, METRICS_PLUGIN_ID);
         saveDefaultConfig();
 
         DiscordSRV.api.subscribe(new DiscordGuildMessagePreProcessListener(this));
